9e185d9aa04b6733a4dd96b71444d88517f9302a
Luiz <Luiz@LUIZ>
2024-08-09 20:02:36 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace TrabalhoFacul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtRuLogin = new System.Windows.Forms.TextBox();
+            this.lblRu = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSenhaLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnFormCadastro = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRuLogin
+            // 
+            this.txtRuLogin.Location = new System.Drawing.Point(179, 56);
+            this.txtRuLogin.Name = "txtRuLogin";
+            this.txtRuLogin.Size = new System.Drawing.Size(174, 20);
+            this.txtRuLogin.TabIndex = 0;
+            // 
+            // lblRu
+            // 
+            this.lblRu.AutoSize = true;
+            this.lblRu.Location = new System.Drawing.Point(176, 40);
+            this.lblRu.Name = "lblRu";
+            this.lblRu.Size = new System.Drawing.Size(23, 13);
+            this.lblRu.TabIndex = 3;
+            this.lblRu.Text = "RU";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TrabalhoFacul.Properties.Resources.account_user_avatar_worker_profile_job_employee_icon_261707;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Senha";
+            // 
+            // txtSenhaLogin
+            // 
+            this.txtSenhaLogin.Location = new System.Drawing.Point(179, 110);
+            this.txtSenhaLogin.Name = "txtSenhaLogin";
+            this.txtSenhaLogin.PasswordChar = 'k';
+            this.txtSenhaLogin.Size = new System.Drawing.Size(174, 20);
+            this.txtSenhaLogin.TabIndex = 17;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(179, 156);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 29);
+            this.btnLogin.TabIndex = 19;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnFormCadastro
+            // 
+            this.btnFormCadastro.Location = new System.Drawing.Point(278, 156);
+            this.btnFormCadastro.Name = "btnFormCadastro";
+            this.btnFormCadastro.Size = new System.Drawing.Size(75, 29);
+            this.btnFormCadastro.TabIndex = 20;
+            this.btnFormCadastro.Text = "Cadastro";
+            this.btnFormCadastro.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Não possui uma conta?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Não possui uma conta?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(410, 230);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFormCadastro);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenhaLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblRu);
+            this.Controls.Add(this.txtRuLogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRuLogin;
+        private System.Windows.Forms.Label lblRu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSenhaLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnFormCadastro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace TrabalhoFacul
+{
+    partial class FormCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastro));
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTelefoneCadastro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSobrenomeCadastro = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDataCadastro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRu = new System.Windows.Forms.Label();
+            this.txtEmailCadastro = new System.Windows.Forms.TextBox();
+            this.txtNomeCadastro = new System.Windows.Forms.TextBox();
+            this.txtRuCadastro = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(158, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Dados";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(531, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Sexo";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(534, 91);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 30;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Feminino";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(534, 68);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 29;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Masculino";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Telefone";
+            // 
+            // txtTelefoneCadastro
+            // 
+            this.txtTelefoneCadastro.Location = new System.Drawing.Point(346, 168);
+            this.txtTelefoneCadastro.Name = "txtTelefoneCadastro";
+            this.txtTelefoneCadastro.Size = new System.Drawing.Size(150, 20);
+            this.txtTelefoneCadastro.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(343, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Sobrenome";
+            // 
+            // txtSobrenomeCadastro
+            // 
+            this.txtSobrenomeCadastro.Location = new System.Drawing.Point(346, 114);
+            this.txtSobrenomeCadastro.Name = "txtSobrenomeCadastro";
+            this.txtSobrenomeCadastro.Size = new System.Drawing.Size(150, 20);
+            this.txtSobrenomeCadastro.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Dara de Nascimento";
+            // 
+            // txtDataCadastro
+            // 
+            this.txtDataCadastro.Location = new System.Drawing.Point(346, 61);
+            this.txtDataCadastro.Name = "txtDataCadastro";
+            this.txtDataCadastro.Size = new System.Drawing.Size(150, 20);
+            this.txtDataCadastro.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Nome";
+            // 
+            // lblRu
+            // 
+            this.lblRu.AutoSize = true;
+            this.lblRu.Location = new System.Drawing.Point(158, 45);
+            this.lblRu.Name = "lblRu";
+            this.lblRu.Size = new System.Drawing.Size(23, 13);
+            this.lblRu.TabIndex = 20;
+            this.lblRu.Text = "RU";
+            // 
+            // txtEmailCadastro
+            // 
+            this.txtEmailCadastro.Location = new System.Drawing.Point(161, 168);
+            this.txtEmailCadastro.Name = "txtEmailCadastro";
+            this.txtEmailCadastro.Size = new System.Drawing.Size(146, 20);
+            this.txtEmailCadastro.TabIndex = 19;
+            // 
+            // txtNomeCadastro
+            // 
+            this.txtNomeCadastro.Location = new System.Drawing.Point(161, 114);
+            this.txtNomeCadastro.Name = "txtNomeCadastro";
+            this.txtNomeCadastro.Size = new System.Drawing.Size(146, 20);
+            this.txtNomeCadastro.TabIndex = 18;
+            // 
+            // txtRuCadastro
+            // 
+            this.txtRuCadastro.Location = new System.Drawing.Point(161, 61);
+            this.txtRuCadastro.Name = "txtRuCadastro";
+            this.txtRuCadastro.Size = new System.Drawing.Size(146, 20);
+            this.txtRuCadastro.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 26);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Cadastro";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 26);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Voltar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TrabalhoFacul.Properties.Resources._3289576_individual_man_people_person_107097;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(111, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 33;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(618, 284);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTelefoneCadastro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSobrenomeCadastro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDataCadastro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRu);
+            this.Controls.Add(this.txtEmailCadastro);
+            this.Controls.Add(this.txtNomeCadastro);
+            this.Controls.Add(this.txtRuCadastro);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCadastro";
+            this.Text = "Cadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTelefoneCadastro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSobrenomeCadastro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDataCadastro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRu;
+        private System.Windows.Forms.TextBox txtEmailCadastro;
+        private System.Windows.Forms.TextBox txtNomeCadastro;
+        private System.Windows.Forms.TextBox txtRuCadastro;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
