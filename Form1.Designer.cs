8158ed7c8bec94ab41a72b857edfdf5d6bebe458
Fernando <fernandoaraujocontato@outlook.com>
2024-08-09 20:49:27 -0300

    mudança de cor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -124,7 +124,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(410, 230);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
