9a29c55977e0888ff3a447ff940a810a0a26b493
Luiz Felipe <luiz19felipe19@hotmail.com>
2024-08-09 21:36:07 -0300

    1

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(229, 175);
+            this.button1.Location = new System.Drawing.Point(251, 177);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(266, 81);
             this.button1.TabIndex = 0;
