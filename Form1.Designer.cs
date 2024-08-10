80c484926de03fcd74904ed58876c0c9622ea176
Luiz Felipe <luiz19felipe19@hotmail.com>
2024-08-09 21:25:51 -0300

    test4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -124,7 +124,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(410, 230);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -135,7 +135,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblRu);
             this.Controls.Add(this.txtRuLogin);
-            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.ForeColor = System.Drawing.Color.Red;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Login";
