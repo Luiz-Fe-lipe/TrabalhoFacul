0ebc663fabfaa22cb7b6a339e464e96572aafa82
Felipe Lima <felip.e.e@hotmail.com>
2024-08-12 21:32:27 -0300

    ajuste nas cores form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,6 @@
             this.txtSenhaLogin = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.btnFormCadastro = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -51,9 +50,11 @@
             // lblRu
             // 
             this.lblRu.AutoSize = true;
+            this.lblRu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRu.ForeColor = System.Drawing.Color.Black;
             this.lblRu.Location = new System.Drawing.Point(176, 40);
             this.lblRu.Name = "lblRu";
-            this.lblRu.Size = new System.Drawing.Size(23, 13);
+            this.lblRu.Size = new System.Drawing.Size(25, 13);
             this.lblRu.TabIndex = 3;
             this.lblRu.Text = "RU";
             // 
@@ -70,9 +71,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(176, 94);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 18;
             this.label1.Text = "Senha";
             // 
@@ -86,6 +89,8 @@
             // 
             // btnLogin
             // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Black;
             this.btnLogin.Location = new System.Drawing.Point(179, 156);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 29);
@@ -95,6 +100,8 @@
             // 
             // btnFormCadastro
             // 
+            this.btnFormCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFormCadastro.ForeColor = System.Drawing.Color.Black;
             this.btnFormCadastro.Location = new System.Drawing.Point(278, 156);
             this.btnFormCadastro.Name = "btnFormCadastro";
             this.btnFormCadastro.Size = new System.Drawing.Size(75, 29);
@@ -103,32 +110,24 @@
             this.btnFormCadastro.UseVisualStyleBackColor = true;
             this.btnFormCadastro.Click += new System.EventHandler(this.btnFormCadastro_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(256, 188);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 13);
-            this.label2.TabIndex = 21;
-            this.label2.Text = "Não possui uma conta?";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(257, 188);
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(275, 186);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(119, 13);
             this.label3.TabIndex = 21;
             this.label3.Text = "Não possui uma conta?";
+            this.label3.UseWaitCursor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ClientSize = new System.Drawing.Size(410, 230);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnFormCadastro);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label1);
@@ -136,7 +135,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblRu);
             this.Controls.Add(this.txtRuLogin);
-            this.ForeColor = System.Drawing.Color.Red;
+            this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Login";
@@ -155,7 +154,6 @@
         private System.Windows.Forms.TextBox txtSenhaLogin;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnFormCadastro;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace TrabalhoFacul
 {
     public partial class Form1 : Form
     {
+
+        public MySqlConnection Conexao { get; set; }
+        string data_source = "datasource=databasepv.cxcs0i2uoy4j.us-east-1.rds.amazonaws.com;database=cadastros;username=admin;password=manga5661;";
+
+
         public Form1()
         {
             InitializeComponent();
@@ -29,6 +35,11 @@ namespace TrabalhoFacul
         {
             AbrirCadastro();
             this.Hide();
+        }
+
+        private void label3_Enter(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.Green;
         }
     }
 }
