b4a39248e3cf40d0cc94923a8ef6d268e9d7be12
Felipe Lima <felip.e.e@hotmail.com>
2024-08-13 22:16:23 -0300

    configurada conexao e validacao de login

--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,9 @@ namespace TrabalhoFacul
     {
         private string ru;
         private string senha;
-
+        private string data_source = "datasource=databasepv.cxcs0i2uoy4j.us-east-1.rds.amazonaws.com;database=cadastros;username=admin;password=manga5661;";
         public MySqlConnection Conexao { get; set; }
-        string data_source = "datasource=databasepv.cxcs0i2uoy4j.us-east-1.rds.amazonaws.com;database=cadastros;username=admin;password=manga5661;";
+        
 
 
         public Form1()
@@ -43,7 +43,44 @@ namespace TrabalhoFacul
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            string ru = txtRuLogin.Text;
+            string senha = txtSenhaLogin.Text;
 
+            if (ValidarLogin(ru, senha))
+            {
+                MessageBox.Show("Bem vindo Fulano");
+                // Redirecione para a próxima tela ou faça outra ação após o login
+            }
+            else
+            {
+                MessageBox.Show("RU ou senha inválidos. Tente novamente.");
+            }
+
+        }
+
+        private bool ValidarLogin(string ru, string senha)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(data_source))
+                {
+                    conn.Open();
+                    string query = "SELECT COUNT(*) FROM usuario WHERE ru = @ru AND senha = @senha";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@ru", ru);
+                        cmd.Parameters.AddWithValue("@senha", senha);
+
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        return count > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar ao banco de dados: " + ex.Message);
+                return false;
+            }
         }
     }
 }
