18c30161fbc9e239f1a270486bb8cf6cf20f3b97
Felipe Lima <felip.e.e@hotmail.com>
2024-08-19 21:32:24 -0300

    ajustados botoes tela exibir e editar

--- Form5.cs
+++ Form5.cs
@@ -19,7 +19,16 @@ namespace TrabalhoFacul
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Form3 formexibir = new Form3();
+            formexibir.Show();
+            this.Close();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form4 formeditar = new Form4();
+            formeditar.Show();
+            this.Close();
         }
     }
 }
