62c41a30b98fdb7847970ef3018fc08029b4b13c
Cleiton <Cleiton@WIN-43IG6EVV8Q5>
2024-08-19 21:16:13 -0300

    adicionada as telas de edição

--- editarCadastro.Designer.cs
+++ editarCadastro.Designer.cs
@@ -28,19 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
-            this.SuspendLayout();
-            // 
-            // Form4
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(597, 322);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form4";
-            this.Text = "Editar";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form4";
         }
 
         #endregion
